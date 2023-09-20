b9f2254100f9a6046aa7e0be6c51ebab42ced882
MdHassanPappu <99087593+MdHassanPappu@users.noreply.github.com>
2023-09-20 09:57:41 +0600

    Update fixed_point.m

--- fixed_point.m
+++ fixed_point.m
@@ -1,31 +1,3 @@
-% Fixed point iteration Method . 
-
-clc
-close all
-syms x
-f = @(x)  exp(x)/3  % e^x-3*x=0
-
-a=input('enter first guess:')
-e=input("enter the tolerence")
-
-n=100
-for i=1:n
-    x=f(a)
-
-        if abs(x-a)<e 
-            fprintf("this is after %f",i)
-             break
-        end
-    a=x
-end 
-
-
-
-
-
-
-
-
 %% newton forword method .  
    
 
